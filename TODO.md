4647aebf731603de28b7c3535de78f426eacd533
mileshenry31 <milesmhenry@icloud.com>
2024-01-12 19:37:53 -0800

    user auth

--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
-1. Search bar
+1. Search bar - almost done, need to make searching so if you search vacum you get vacuum
 2. User auth
 3. Wish list
 4. Rating system
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   get '/search_item', to: 'search#search_item'
+  root "items#index"
 end
